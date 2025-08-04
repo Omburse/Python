9374a74ff864a972634f62998fb0c462ae811604
OM ULHAS BURSE <omburse0019@gmail.com>
2025-08-04 23:33:06 +0530

    Variable in Python

---
+++ Var1.py
@@ -0,0 +1,17 @@
+"""
+a = 5
+b = 10
+c = 25.55
+print(type(a))
+print(type(b))
+print(type(c))
+print("My age is",a)
+print("Om Ulhas Burse")
+"""
+
+# Sum of two Numbers
+
+A = 13
+B = 10
+C = A+B
+print("The value of C is",C)
