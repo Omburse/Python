f3da8b2c617ceebe9997048c55e3661ab5e9ceb0
OM ULHAS BURSE <omburse0019@gmail.com>
2025-08-04 23:33:51 +0530

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,30 @@
+#Arithmetic Operators
+a = 13
+b = 22
+c = a + b
+c3 = a - b
+c2 = a * b
+c1 = a / b
+print("The value of a div by b is ",c1)
+print("The value of a Mul by b is ",c2)
+print("The value of a Min b is ",c3)
+print("The value of a plus b is ",c)
+
+#Relation | Comparision Operators
+
+A = 20
+B = 25
+C = (A==B)
+C1 = (A!=B)
+C2 = (A>=B)
+C3 = (A<=B)
+print("A is equal to B ",C)
+print("A is not equal to B",C1)
+print("A is grater than B",C2)
+print("A is less than B",C3)
+
+#My Practise
+A23 = 166
+B22 = 765
+C19 = A23/B22
+print("The value is " , C19)
